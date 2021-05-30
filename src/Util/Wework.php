1dc47872367faf03d1f06fc7b57d652c3dd5b06c
luish <974517230@qq.com>
2021-05-30 13:49:44 +0800

    提交

---
+++ src/Util/Wework.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Luish\FirstLib\Util;
+use GuzzleHttp\Client;
+
+/**
+ * error code 说明.
+ * <ul>
+ *    <li>-40001: 签名验证错误</li>
+ *    <li>-40002: xml解析失败</li>
+ *    <li>-40003: sha加密生成签名失败</li>
+ *    <li>-40004: encodingAesKey 非法</li>
+ *    <li>-40005: corpid 校验错误</li>
+ *    <li>-40006: aes 加密失败</li>
+ *    <li>-40007: aes 解密失败</li>
+ *    <li>-40008: 解密后得到的buffer非法</li>
+ *    <li>-40009: base64加密失败</li>
+ *    <li>-40010: base64解密失败</li>
+ *    <li>-40011: 生成xml失败</li>
+ * </ul>
+ */
+class Wework {
+    public  function curl() {
+       $s =  new Client([
+               'base_uri' => "http://www.baidu.com",
+               'timeout'  =>5,
+           ]
+           );
+       return $s->get('',[]);
+    }
+}
